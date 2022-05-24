5f2d90fe9da9fadb90f1560e5a8f14b4e83ec603
ikatahira <isaque.katahira@fatec.sp.gov.br>
2022-05-24 17:57:01 +0000

    criando o models

--- src/main/java/application/models/Genero.java
+++ src/main/java/application/models/Genero.java
@@ -17,7 +17,7 @@ public class Genero {
     private int id;
     private String nome;
 
-    @OneToMany(mappedBy = "genero")
+    @OneToMany(mappedBy = "generos")
     private Set<Jogo> jogos = new HashSet<>();
 
     public void setId(int id) {
